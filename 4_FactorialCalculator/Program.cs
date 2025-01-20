648462a28aac2076cda858135cc7b75f1a419691
lance <lance@BONBON>
2025-01-20 22:54:29 +0800

    Add project files.

---
+++ 4_FactorialCalculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_FactorialCalculator
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+            int num;
+
+            if (int.TryParse(input, out num) && num >= 0)
+            {
+                long factorial = 1;
+                for ( int i = 1; i <= num; i++ )
+                {
+                    factorial *= i;
+                }
+                Console.WriteLine($"The factorial of {num} is {factorial}.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid! Enter a non-negative integer.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
